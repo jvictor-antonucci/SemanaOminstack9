c2b5fc52a1f393ee6c715703eddfb9e5f0929dd7
João Victor Antonucci <jvictor.antonucci@gmail.com>
2020-03-02 16:42:43 -0300

    Aplicação AirCnC

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+// index, show, store, update, destroy únicos métodos que deveriam estar dentro de um controller
+const User = require('../models/User');
+
+module.exports = {
+    async store(req, res) {
+        const { email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ email });
+        }
+
+        return res.json(user);
+    }
+};
---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://jvictor:jvictor@aircnc-blmrr.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar route params (para edição, delete)
+//req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+app.listen(3333);
